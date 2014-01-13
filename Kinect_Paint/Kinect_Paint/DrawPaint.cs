5570c50248d28ae38f27f583ce6163456e292a70
S1123043 <s1123043@cce.kanagawa-it.ac.jp>
2014-01-13 21:04:35 +0900

    Kinect Paintを追加

---
+++ Kinect_Paint/Kinect_Paint/DrawPaint.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Imaging;
+using System.Windows;
+using Microsoft.Kinect;
+using System.Windows.Media;
+
+namespace Microsoft.Samples.Kinect.SkeletonBasics
+{
+    class DrawPaint
+    {
+        public RenderTargetBitmap target = new RenderTargetBitmap(640, 480, 96f, 96f, PixelFormats.Default);
+        DrawingVisual dv = new DrawingVisual();
+
+        public RenderTargetBitmap DrawOrangeDot(Point jointPoint)
+        {
+            using (DrawingContext dc = dv.RenderOpen())
+            {
+                // オレンジの矩形を描く
+                dc.DrawRectangle(Brushes.Orange, null, new Rect(jointPoint.X, jointPoint.Y, 10.0, 10.0));
+                dc.Close();
+            }
+
+            target.Render(dv);
+            return target;
+        }
+
+        public RenderTargetBitmap DrawSkyBlueDot(Point jointPoint)
+        {
+            using (DrawingContext dc = dv.RenderOpen())
+            {
+                // 水色の矩形を描く
+                dc.DrawRectangle(Brushes.SkyBlue, null, new Rect(jointPoint.X, jointPoint.Y, 10.0, 10.0));
+                dc.Close();
+            }
+
+            target.Render(dv);
+            return target;
+        }
+
+        public void ClearDot()
+        {
+            target.Clear();
+        }
+    }
+}
---
+++ Kinect_Paint/Kinect_Paint/PictureShot.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Windows;
+
+
+namespace Microsoft.Samples.Kinect.SkeletonBasics
+{
+    class PictureShot : DrawPaint
+    {
+        // 画像を保存する
+        public void ShotPicture()
+        {
+
+            // create a png bitmap encoder which knows how to save a .png file
+            BitmapEncoder encoder = new PngBitmapEncoder();
+
+            // create frame from the writable bitmap and add to encoder
+            encoder.Frames.Add(BitmapFrame.Create(target));
+
+            string time = System.DateTime.Now.ToString("hh'-'mm'-'ss", CultureInfo.CurrentUICulture.DateTimeFormat);
+
+            string myPhotos = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            string path = Path.Combine(myPhotos, "Image" + time + ".png");
+
+            // write the new file to disk
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Create))
+                {
+                    encoder.Save(fs);
+                }
+
+                MessageBox.Show("[" + path + "]に画像を書き出しました。", "完了ダイアログ", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("画像の書き出しに失敗しました。", "画像の書き出しエラー", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
